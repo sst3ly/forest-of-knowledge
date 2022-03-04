a5601996c3aec516b7408cfe60cb1529eeeacd38
sst3ly <69128003+sst3ly@users.noreply.github.com>
2022-03-03 17:18:53 -0800

    Checking visibility

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+document.getElementsByClassName("L2").style.visibility = "hidden"
+
 function selectionL1(selection){
   if(selection == "math"){
     alert("Selected math!")
+    document.getElementById("L2Smth").style.visibility="visible"
   }
 }
