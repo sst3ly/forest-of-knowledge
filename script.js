a0e1c198d4dc28ed1715b4300ff21aeb7cc99675
sst3ly <69128003+sst3ly@users.noreply.github.com>
2023-03-25 01:38:29 +0000

    modified:   script.js

--- script.js
+++ script.js
@@ -1,57 +1,57 @@
-document.getElementsByClassName("L2").style.display = "none"
+document.getElementsByClassName("L2").style.visibility = "hidden"
 
 function selectionL1(selection){
   if(selection == "math"){
-    if(document.getElementById("L2Smth").style.display == "block"){
-      document.getElementById("L2Smth").style.display == "none"
+    if(document.getElementById("L2Smth").style.visibility == "visible"){
+      document.getElementById("L2Smth").style.visibility == "hidden"
     }
     else{
-      document.getElementById("L2Smth").style.display="block"
+      document.getElementById("L2Smth").style.visibility="visible"
     }
   }
   if(selection == "sci"){
-    if(document.getElementById("L2Ssci").style.display == "block"){
-      document.getElementById("L2Ssci").style.display == "none"
+    if(document.getElementById("L2Ssci").style.visibility == "visible"){
+      document.getElementById("L2Ssci").style.visibility == "hidden"
     }
     else{
-      document.getElementById("L2Ssci").style.display="block"
+      document.getElementById("L2Ssci").style.visibility="visible"
     }
   }
   if(selection == "hist"){
-    if(document.getElementById("L2hist").style.display == "block"){
-      document.getElementById("L2hist").style.display == "none"
+    if(document.getElementById("L2hist").style.visibility == "visible"){
+      document.getElementById("L2hist").style.visibility == "hidden"
     }
     else{
-      document.getElementById("L2Shist").style.dispaly="block"
+      document.getElementById("L2Shist").style.dispaly="visible"
     }
   }
   if(selection == "inet"){
-    if(document.getElementById("L2Snet").style.display == "block"){
-      document.getElementById("L2Snet").style.display == "none"
+    if(document.getElementById("L2Snet").style.visibility == "visible"){
+      document.getElementById("L2Snet").style.visibility == "hidden"
     }
     else{
-      document.getElementById("L2Snet").style.display = "block"
+      document.getElementById("L2Snet").style.visibility = "visible"
     }
   }
   if(selection == "edu"){
-    if(document.getElementById("L2Sedu").style.display == "block"){
-      document.getElementById("L2Sedu").style.display == "none"
+    if(document.getElementById("L2Sedu").style.visibility == "visible"){
+      document.getElementById("L2Sedu").style.visibility == "hidden"
     }
   }
   if(selection == "tech"){
-    if(document.getElementById("L2Stch").style.display == "block"){
-      document.getElementById("L2Stch").style.display == "none"
+    if(document.getElementById("L2Stch").style.visibility == "visible"){
+      document.getElementById("L2Stch").style.visibility == "hidden"
     }
     else{
-      document.getElementById("L2Stch").style.display == "block"
+      document.getElementById("L2Stch").style.visibility == "visible"
     }
   }
   if(selection == "wrld"){
-    if(document.getElementById("L2Stch").style.display == "block"){
-      document.getElementById("L2Swrld").style.display == "none"
+    if(document.getElementById("L2Stch").style.visibility == "visible"){
+      document.getElementById("L2Swrld").style.visibility == "hidden"
     }
     else{
-      document.getElementById("L2Swrld").style.display == "block"
+      document.getElementById("L2Swrld").style.visibility == "visible"
     }
   }
 }
